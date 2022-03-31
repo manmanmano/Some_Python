64d2449619e7211b35689b4af04ca6bb9d608567
manmanmano <jimiskools@gmail.com>
2022-03-31 18:28:42 +0300

    dashed line

--- Exercises/GUI/Turtle/main.py
+++ Exercises/GUI/Turtle/main.py
@@ -6,5 +6,12 @@ for _ in range(4):
     timmy.forward(100)
     timmy.right(90)
 
+for _ in range(20):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+
 screen = Screen()
 screen.exitonclick()
